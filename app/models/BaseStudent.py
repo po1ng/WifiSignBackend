6a00fbb96a5646541045aec3ecaa1322d7a0602b
gogoforit <715157026@qq.com>
2017-05-21 17:49:54 +0800

    创建了BaseStudent类，包含查询学生的基础信息

---
+++ app/models/BaseStudent.py
@@ -0,0 +1,24 @@
+from app.models.MongodbConn import MongoPipeline
+
+class BaseStudent():
+
+    def __init__(self,name):
+        self.student_info = self.query_basic_info(name=name)
+        self.mac = self.student_info['mac']
+        self.name = self.student_info['name']
+        self.student_id = self.student_info['studentid']
+        self.class_id = self.student_info['class_num']
+
+
+    def query_basic_info(self,name):
+        conn = MongoPipeline()
+        conn.open_connection('qiandao_mac_name')
+        student_info = conn.getIds_one('info',{'name':name})
+        return student_info
+
+
+if __name__ == '__main__':
+    student = BaseStudent('万仕贤')
+    print(student.name,student.mac,student.class_id,student.student_id)
+
+
