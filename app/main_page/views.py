7f3976d780c34942811dad959e8e364884b92b1d
gogoforit <715157026@qq.com>
2017-05-28 19:35:02 +0800

    初步使用Ajax

--- app/main_page/views.py
+++ app/main_page/views.py
@@ -1,6 +1,7 @@
 from app.main_page import main_page as main_page_Blueprint
 from app.models.BaseClass import BaseClass
 from flask import  render_template
+from flask import jsonify
 
 @main_page_Blueprint.route('/')
 def main_page():
@@ -19,3 +20,16 @@ def teacher():
         dic_all_students[student] = 0
     print(dic_all_students)
     return render_template('teacher_home.html',dic_all_students = dic_all_students)
+
+@main_page_Blueprint.route('/data')
+def data():
+    Class = BaseClass(21, 5, '2017-05-20')
+
+    list_sign_students = Class.list_sign_students
+    list_unsign_students = Class.list_unsign_students
+    dic_all_students = {}
+    for student in list_sign_students:
+        dic_all_students[student] = 1
+    for student in list_unsign_students:
+        dic_all_students[student] = 0
+    return jsonify(dic_all_students = dic_all_students)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app import create_app
 
 app = create_app()
-app.run(debug=True,port=8086)
+app.run(debug=True,port=8087)
