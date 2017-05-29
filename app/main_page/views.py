db419fe5d1ca8aaf2f270c441afdd3de4b5b075b
gogoforit <715157026@qq.com>
2017-05-29 17:11:40 +0800

    修改了教师页面，修改了状态颜色，按钮的默认大小

--- app/main_page/views.py
+++ app/main_page/views.py
@@ -2,15 +2,17 @@ from app.main_page import main_page as main_page_Blueprint
 from app.models.BaseClass import BaseClass
 from flask import  render_template
 from flask import jsonify
+from config import CLASS_NUMBER
+class_number = CLASS_NUMBER
 
 @main_page_Blueprint.route('/')
 def main_page():
-    Class = BaseClass(21,5,'2017-05-20')
+    Class = BaseClass(class_number)
     return render_template('index.html',Class=Class)
 
 @main_page_Blueprint.route('/teacher')
 def teacher():
-    Class = BaseClass(21)
+    Class = BaseClass(class_number)
     list_sign_students = Class.list_sign_students
     list_unsign_students = Class.list_unsign_students
     dic_all_students = {}
@@ -23,7 +25,7 @@ def teacher():
 
 @main_page_Blueprint.route('/data')
 def data():
-    Class = BaseClass(21)
+    Class = BaseClass(class_number)
 
     list_sign_students = Class.list_sign_students
     list_unsign_students = Class.list_unsign_students
@@ -32,5 +34,4 @@ def data():
         dic_all_students[student] = 1
     for student in list_unsign_students:
         dic_all_students[student] = 0
-    print(dic_all_students)
     return jsonify(dic_all_students)
