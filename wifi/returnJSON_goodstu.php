ef9428dcbed00dc064661afe48c7e5977f2dc9f4
No-sleeping <864830804@qq.com>
2017-06-03 01:11:08 +0800

    添加了图表（2）

---
+++ wifi/returnJSON_goodstu.php
@@ -0,0 +1,6 @@
+<?php
+    $arr = array("小明"=>"./1.html","小李"=>"./2.html","小王"=>"./3.html","小吴"=>"./4.html","小赵"=>"./5.html","小黄"=>"./6.html","小钱"=>"./7.html");
+    $json=json_encode($arr);
+    echo $json;
+    //echo 'ok';
+?>
