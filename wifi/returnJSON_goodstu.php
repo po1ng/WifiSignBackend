bb3716cdb94f8b378064a5b7033ca7479ac500e1
No-sleeping <864830804@qq.com>
2017-06-03 14:09:13 +0800

    添加吴的修改文件

--- wifi/returnJSON_goodstu.php
+++ wifi/returnJSON_goodstu.php
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 <?php
     $arr = array("小明"=>"./1.html","小李"=>"./2.html","小王"=>"./3.html","小吴"=>"./4.html","小赵"=>"./5.html","小黄"=>"./6.html","小钱"=>"./7.html");
     $json=json_encode($arr);
     echo $json;
     //echo 'ok';
+=======
+<?php
+    $arr = array("小明"=>"./1.html","小李"=>"./2.html","小王"=>"./3.html","小吴"=>"./4.html","小赵"=>"./5.html","小黄"=>"./6.html","小钱"=>"./7.html");
+    $json=json_encode($arr);
+    echo $json;
+    //echo 'ok';
+>>>>>>> d188c8194268130c418852f265f15c566ca3f538
 ?>
