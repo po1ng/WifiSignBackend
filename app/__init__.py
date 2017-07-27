9783d3f068b39bd568a7bd8c7654f992866a2aa0
gogoforit <715157026@qq.com>
2017-07-27 11:16:28 +0800

    修改了flask框架的配置文件

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mongoengine import MongoEngine
@@ -7,7 +8,12 @@ from flask_login import LoginManager
 UPLOAD_FOLDER = './static/doc'
 db = MongoEngine()
 login_manager = LoginManager()
-
+app_secret_key = os.environ['APP_SECRET_KEY']
+mongo_db = os.environ['MONGO_DB']
+mongo_host = os.environ['MONGO_HOST']
+mongo_port = os.environ['MONGO_PORT']
+mongo_username = os.environ['MONGO_USERNAME']
+mongo_password = os.environ['MONGO_PASSWORD']
 
 def create_app():
     app = Flask(__name__)
@@ -16,16 +22,16 @@ def create_app():
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER  # 设置文件上传的目标文件夹
     app.config['SECRET_KEY'] = 'hard to guess'
     app.config['MONGODB_SETTINGS'] = {  # 配置MongoDB
-        'db': 'web',
-        'host': '127.0.0.1',
-        'port': 27017,
-        'username':'pipi',
-        'password':'123456'
+        'db': mongo_db,
+        'host': mongo_host,
+        'port': int(mongo_port),
+        'username':mongo_username,
+        'password':mongo_password
     }
     app.session_interface = MongoEngineSessionInterface(db)
     db.init_app(app)
 
-    app.secret_key = 's3cr3t'
+    app.secret_key = app_secret_key
     login_manager.session_protection = 'strong'
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
