ed92c40018b0ce945b6bed788fea323e473a5bbd
gogoforit <715157026@qq.com>
2017-08-05 00:31:23 +0800

    修改了用户注册页面

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,42 +8,42 @@ from wtforms.validators import Email
 from wtforms.validators import EqualTo
 from wtforms.validators import Length
 from wtforms.validators import Regexp
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 from app.models.BaseUser import User
 
 
 class LoginForm(Form):
-    email = StringField('Email', validators=[Required(), Length(1,64), Email()])
-    password = PasswordField('Password', validators=[Required()])
+    email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
 
 
 class RegistrationForm(Form):
-    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField('Username', validators=[
-                           Required(), Length(1, 64),
+                           DataRequired(), Length(1, 64),
                            Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                            'Usernames must have only letters, '
                            'numbers, dots or underscores')])
     password = PasswordField('Password',
-                             validators=[Required(),
+                             validators=[DataRequired(),
                              EqualTo('password2', 'Password must match')])
-    password2 = PasswordField('Confirm Password', validators=[Required()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
     class_number = StringField('Class Number',
-                               validators=[Required(),
+                               validators=[DataRequired(),
                                Regexp('^[0-9]',0,'Class number must have only numbers')])
     nickname = StringField('Nickname',
-                           validators=[Required(), Length(1, 64),
+                           validators=[DataRequired(), Length(1, 64),
                            Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                            'Nickname must have only letters, '
                            'numbers, dots or underscores')])
 
-    def validate_email(self, field):
-        if User.objects(email=field.data).first():
-            raise ValidationError('Email already registered.')
+    def validate_email(self, email):
+        if User.objects(email=email).first():
+            return True
 
-    def validate_nickname(self, field):
-        if User.objects(nickname=field.data).first():
-            raise ValidationError('Nickname already in use.')
+    def validate_nickname(self, nickname):
+        if User.objects(nickname=nickname).first():
+            return True
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,14 @@
-from .forms import LoginForm,RegistrationForm
-from flask import redirect
-from flask import render_template
-from flask import url_for
 from flask import flash
+from flask import jsonify
+from .forms import LoginForm, RegistrationForm
+from flask import render_template
 from flask_login import current_user
 from flask_login import login_user
 from flask_login import logout_user
 from flask_login import login_required
+from app.utils import response_dict
 from app.models.BaseUser import User
+from app.constants import EMAIL_USED, NICKNAME_USED, SUCCESS
 from . import auth as auth_Blueprint
 
 
@@ -26,32 +27,38 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.objects(email = form.email.data).first()
+        user = User.objects(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user,form.remember_me.data)
-            return redirect('/main_page/student/'+user.username)
-    return render_template('login.html',form=form,user = current_user)
+            login_user(user, form.remember_me.data)
+            return '登录失败'
+    return '登录成功'
 
 
 @auth_Blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email=form.email.data, username=form.username.data,
+    if form.validate:
+        user = User(email=form.email.data,
+                    username=form.username.data,
                     class_number=form.class_number.data,
                     nickname=form.nickname.data)
         user.password = form.password.data
-        user.save()
-        flash('注册成功！可以登录了！')
-        return redirect(url_for('auth.login'))
+        if form.validate_email(form.email.data):
+            return jsonify(response_dict(EMAIL_USED))
+        elif form.validate_nickname(form.nickname.data):
+            return jsonify(response_dict(NICKNAME_USED))
+        else:
+            return jsonify(response_dict(SUCCESS))
+    else:
+        return '注册失败！'
 
-    return render_template('register.html', form=form)
 
 
 @auth_Blueprint.route('/logout')
-@login_required
+# @login_required
 def logout():
-    logout_user()
-    flash('You have been logged out.')
-    return render_template('index.html')
+    # logout_user()
+    # flash('You have been logged out.')
+    # return render_template('index.html')
+    return 'hello logout'
 
